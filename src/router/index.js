f612742ce2f660b2d19b03b41893f0fb02b9a1fb
luojiawei <luojiawei@chaolian360.com>
2020-08-14 18:54:03 +0800

    前端框架

--- src/router/index.js
+++ src/router/index.js
@@ -13,3 +13,23 @@ export default new Router({
     }
   ]
 })
+export  const  RouterMap = [{
+
+},{}
+]
+export const constantRouterMap = [
+  {path: '/login', component: () => import('@/views/login/index'), hidden: true},
+  {path: '/404', component: () => import('@/views/404'), hidden: true},
+  {
+    path: '版本2',
+    component: Layout,
+    redirect: '/home',
+    children: [{
+      path: 'home',
+      name: 'home',
+      component: () => import('@/views/home/index'),
+      meta: {title: '首页', icon: 'home'}
+    }]
+  }
+]
+
---
+++ src/view/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 class = "test">
+      {{msg}}}
+    </h1>
+  </div>
+</template>
+<script>
+  export  default {
+    name:"test",
+    data(){
+      return "test";
+    }
+
+  }
+</script>
